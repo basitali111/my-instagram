e3112c745c581b1c4c03269ee96f00ce5f966871
basitali111 <allahbadshah148@gmail.com>
2023-05-20 23:03:22 +0500

    Add post profile photo and menu button for edit and delete

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts
-  has_one_attached :profile_picture
+  has_one_attached :profile_pic
 end
