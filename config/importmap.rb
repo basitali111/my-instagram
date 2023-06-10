ca7eadedc2bfe2558bdd4cb98880f4b30472e785
basitali111 <allahbadshah148@gmail.com>
2023-06-10 15:36:32 +0500

    Add compelte code of filepond and now its working

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,4 @@ pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.e
 pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.7/dist/esm/index.js
 "
 pin "filepond", to: "https://ga.jspm.io/npm:filepond@4.30.4/dist/filepond.js"
+pin_all_from "app/javascript/custom", under: "custom"
