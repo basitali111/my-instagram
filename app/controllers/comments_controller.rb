c844356c375de74435639ca35190d027b0e2721b
basitali111 <allahbadshah148@gmail.com>
2023-06-23 23:12:10 +0500

    Add comment model to view all comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,21 @@ class CommentsController < ApplicationController
   before_action :set_post, only: [:create]
   def create
     @comment = @post.comments.create(user: current_user, body: params[:comment_body])
+  respond_to do |format|
+       format.turbo_stream do
+        render turbo_stream: turbo_stream.replace(
+          "post#{@post.id}comments",
+           partial: "posts/post_comments", 
+           locals: { post: @post })
+       end
+    end
   end
 
   def destroy
     @comment = Comment.find(params[:id])
-    return unless @comment.user == current_user
-
+   if(@comment.user == current_user)
     @comment.destroy
+   end
   end
 
   private
