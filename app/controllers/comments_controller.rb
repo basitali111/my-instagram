f2e93774aa32ef0b0ffb35a9dbaff0c6f303c228
basitali111 <allahbadshah148@gmail.com>
2023-06-23 23:44:48 +0500

    Add comment with its text body and when created at in comment model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,21 +2,22 @@ class CommentsController < ApplicationController
   before_action :set_post, only: [:create]
   def create
     @comment = @post.comments.create(user: current_user, body: params[:comment_body])
-  respond_to do |format|
-       format.turbo_stream do
+    respond_to do |format|
+      format.turbo_stream do
         render turbo_stream: turbo_stream.replace(
           "post#{@post.id}comments",
-           partial: "posts/post_comments", 
-           locals: { post: @post })
-       end
+          partial: 'posts/post_comments',
+          locals: { post: @post }
+        )
+      end
     end
   end
 
   def destroy
     @comment = Comment.find(params[:id])
-   if(@comment.user == current_user)
+    return unless @comment.user == current_user
+
     @comment.destroy
-   end
   end
 
   private
