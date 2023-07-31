d1aa51a0e83bdff93947f9fec4a0136571cccfda
basitali111 <allahbadshah148@gmail.com>
2023-07-31 15:41:37 +0500

    Fix linters error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-   redirect_to root_path
+    redirect_to root_path
   end
 
   # GET /posts/1 or /posts/1.json
@@ -47,16 +47,18 @@ class PostsController < ApplicationController
 
   # DELETE /posts/1 or /posts/1.json
   def destroy
+    return if current_user != @post.user
+
     @post.likes.destroy_all
     @post.comments.destroy_all
 
-  # Now you can safely destroy the post
-  @post.destroy
+    # Now you can safely destroy the post
+    @post.destroy
 
-  respond_to do |format|
-    format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-    format.json { head :no_content }
-  end
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
