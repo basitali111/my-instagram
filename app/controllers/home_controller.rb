727b4f57bd46579d9126e7bbaded8b5742fb7f8e
basitali111 <allahbadshah148@gmail.com>
2023-08-12 00:01:04 +0500

    Add suggestion

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,15 @@
 class HomeController < ApplicationController
+  before_action :set_suggestions
   def index; end
-end
+
+  
+    def set_suggestions
+      @suggestions = [current_user.followers]
+      [current_user.followers,current_user.followings].flatten.uniq.each do |f|
+        @suggestions.append([f.followers,f.followings])
+      end
+      @suggestion = [@suggestions,User.all.sapmle(10)].flatten.uniq - [current_user.followings,current_user].flatten
+      @suggestion = @seggustion.sample(5)
+    end
+ end
+
