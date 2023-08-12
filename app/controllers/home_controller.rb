2b965a355625949de4e1f29b21777f65ac43e00f
basitali111 <allahbadshah148@gmail.com>
2023-08-12 23:47:10 +0500

    fix linters error

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,14 +2,12 @@ class HomeController < ApplicationController
   before_action :set_suggestions
   def index; end
 
-  
-    def set_suggestions
-      @suggestions = [current_user.followers]
-      [current_user.followers,current_user.followings].flatten.uniq.each do |f|
-        @suggestions.append([f.followers,f.followings])
-      end
-      @suggestion = [@suggestions,User.all.sapmle(10)].flatten.uniq - [current_user.followings,current_user].flatten
-      @suggestion = @seggustion.sample(5)
+  def set_suggestions
+    @suggestions = [current_user.followers]
+    [current_user.followers, current_user.followings].flatten.uniq.each do |f|
+      @suggestions.append([f.followers, f.followings])
     end
- end
-
+    @suggestion = [@suggestions, User.all.sapmle(10)].flatten.uniq - [current_user.followings, current_user].flatten
+    @suggestion = @seggustion.sample(5)
+  end
+end
