b45476cdb65849284142de1a5246a5ff61c44912
basitali111 <allahbadshah148@gmail.com>
2023-08-14 23:56:20 +0500

    Add user search menu

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
   before_action :set_user
+
+  def index
+    if params[:search_query].present?
+      @users = User.search(params[:search_query])
+    else
+      @users = User.all
+    end
+  end
   def show; end
 
   private
